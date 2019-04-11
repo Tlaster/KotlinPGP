3c7443b20c399cc4ab5326fa5333accf484620fd
Tlaster <tlaster@outlook.com>
2019-04-11 16:00:32 +0800

    Add some extensions

--- src/main/kotlin/extensions.kt
+++ src/main/kotlin/extensions.kt
@@ -1,9 +1,12 @@
 package moe.tlaster.kotlinpgp
 
+import moe.tlaster.kotlinpgp.utils.UserIdUtils
 import org.bouncycastle.bcpg.ArmoredOutputStream
 import org.bouncycastle.openpgp.PGPKeyRing
+import org.bouncycastle.openpgp.PGPPublicKey
+import org.bouncycastle.openpgp.PGPPublicKeyRing
+import org.bouncycastle.openpgp.operator.jcajce.JcaKeyFingerprintCalculator
 import java.io.ByteArrayOutputStream
-
 
 fun PGPKeyRing.exportToString(): String {
     return ByteArrayOutputStream().use {
@@ -27,3 +30,26 @@ val String.isPGPPublicKey
 
 val String.isPGPPrivateKey
     get() = this.startsWith("-----BEGIN PGP PRIVATE KEY BLOCK-----") && this.endsWith("-----END PGP PRIVATE KEY BLOCK-----")
+
+val PGPPublicKey.name
+    get() = if (userIDs.hasNext()) {
+        UserIdUtils.splitUserId(userIDs.next()).name
+    } else {
+        ""
+    }
+
+val PGPPublicKey.email
+    get() = if (userIDs.hasNext()) {
+        UserIdUtils.splitUserId(userIDs.next()).email
+    } else {
+        ""
+    }
+
+fun PGPKeyRing.extractPublicKeyRing(): PGPPublicKeyRing {
+    val it = publicKeys
+    val stream = ByteArrayOutputStream(2048)
+    while (it.hasNext()) {
+        stream.write(it.next().encoded)
+    }
+    return PGPPublicKeyRing(stream.toByteArray(), JcaKeyFingerprintCalculator())
+}
